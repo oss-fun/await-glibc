ddc67c2777e88bdb4b2e3ca7c4344202cf0b4821
materialofmouse <materialofmouse@gmail.com>
2024-12-13 07:11:26 +0900

    scriptsで定義していたディレクトリの存在確認が間違っていたので修正

--- rootfs-builder/scripts/download_rootfs.sh
+++ rootfs-builder/scripts/download_rootfs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source /app/config/base_rootfs.conf
 echo "${DIST}" "${NAME}"
 
 cd /output
-if [ ! -d await-rootfs ]; then
+if [ -d /output/await-rootfs ]; then
 	rm -rf await-rootfs
+	echo "await-rootfs is exist. remove await-rootfs"
 fi
 
 debootstrap --arch=amd64 "${DIST}" "${NAME}"
