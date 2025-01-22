4d1bb73feed1c7550a1c344bf4220d822058226c
materialofmouse <materialofmouse@gmail.com>
2025-01-22 17:54:52 +0900

    テストコードを追加

--- glibc-builder/glibc-2.35/sysdeps/unix/sysv/linux/test-preopen-linux.c
+++ glibc-builder/glibc-2.35/sysdeps/unix/sysv/linux/test-preopen-linux.c
@@ -1,15 +1,3 @@
-/* Test that preopen functionality works correctly in open64.
-	 Linux version.
-
-	 Copyright (C) 2024 Free Software Foundation, Inc.
-	 This file is part of the GNU C Library.
-
-	 The GNU C Library is free software; you can redistribute it and/or
-	 modify it under the terms of the GNU Lesser General Public
-	 License as published by the Free Software Foundation; either
-	 version 2.1 of the License, or (at your option) any later version.
-	 */
-
 #include <array_length.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -19,56 +7,50 @@
 #include <string.h>
 #include <unistd.h>
 
-/* Return true if FD is contained in the array [FDS, FDS + COUNT].  */
-	static bool
-check_fd_in_list (int fd, int *fds, size_t count)
-{
-	for (size_t i = 0; i < count; ++i)
-		if (fds[i] == fd)
-			return true;
-	return false;
+//#include <sysdeps/unix/sysv/linux/open64.c>
+//extern int preopen(const char *file, int oflag, int mode, int *last_errno);
+
+static int test_preopen(const char *file, int oflag, int mode, int *last_errno) {
+	int res = open(file, oflag, mode, last_errno);
+	if (res > 0) return 0;
+	else return -1;
 }
 
-#define test_preopen(experr, path, flags)                        \
-	(__extension__ ({                                             \
-									errno = 0xdead;                                            \
-									int expected_fd[] = experr;                                \
-									int ret = preopen (path, flags);                           \
-									int err = errno;                                           \
-									int fail;                                                  \
-									if (check_fd_in_list (ret, expected_fd, array_length (expected_fd))) \
-									fail = 0;                                                \
-									else                                                       \
-									{                                                        \
-									fail = 1;                                             \
-									printf ("FAIL: preopen: path=%s expected one of %s, got fd=%d\n", \
-											path, #experr, ret);                           \
-											}                                                        \
-											fail;                                                      \
-											}))
 
-	static int
-do_test (void)
-{
+static int do_test (void) {
 	int fails = 0;
+	const char* test_path[] = {"/tmp", "/tmp/hoge", "/tmp/huga", "/tmp/sample.txt"};
+	int fd, i;
+	char fd_str[256];
+	char path_str[1024];
+	int fd_str_pos = 0;
+	int path_str_pos = 0;
 
-	// 環境変数の設定
-	setenv("PREOPEN_FDS", "3:4:5", 1);
-	setenv("PREOPEN_PATHS", "/usr/local:/tmp:/home", 1);
+	for (i = 0; i < sizeof(test_path) / sizeof(test_path[0]); i++) {
+		fd = open(test_path[i], O_RDWR | O_CREAT, 0644);
+		if (fd < 0) {
+			printf("Failed to open %s: %s\n", test_path[i], strerror(errno));
+			return 1;
+		}
 
-	// テストケース1: マッチするパス
-	fails |= test_preopen (LIST(3), "/usr/local/test.txt", O_RDONLY);
+		if (i > 0) {
+			fd_str_pos += snprintf(fd_str + fd_str_pos, sizeof(fd_str) - fd_str_pos, ":");
+		}
+		fd_str_pos += snprintf(fd_str + fd_str_pos, sizeof(fd_str) - fd_str_pos, "%d", fd);
 
-	// テストケース2: マッチしないパス
-	fails |= test_preopen (LIST(-1), "/var/log/test.txt", O_RDONLY);
+		if (i > 0) {
+			path_str_pos += snprintf(path_str + path_str_pos, sizeof(path_str) - path_str_pos, ":");
+		}
+		path_str_pos += snprintf(path_str + path_str_pos, sizeof(path_str) - path_str_pos, "%s", test_path[i]);
+	}
 
-	// テストケース3: マッチする別のパス
-	fails |= test_preopen (LIST(4), "/tmp/test.txt", O_RDONLY);
+	setenv("PREOPEN_PATHS", path_str, 1);
+	setenv("PREOPEN_FDS", fd_str, 1);
 
-	// テストケース4: 相対パス（マッチしない）
-	fails |= test_preopen (LIST(-1), "local/test.txt", O_RDONLY);
-
+	fails |= test_preopen ("/tmp/sample.txt", O_RDONLY, 0, 0);
+	fails |= test_preopen ("sample.txt", O_RDONLY, 0, 0);
+	fails |= test_preopen ("hoge/sample.txt", O_RDONLY, 0, 0);
+	fails |= test_preopen ("/etc/passwd", O_RDONLY, 0, 0);
 	return fails;
 }
-
 #include "support/test-driver.c"
